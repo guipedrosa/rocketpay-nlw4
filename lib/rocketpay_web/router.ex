fca0c5d50fe213d108aea678ad6141bcac8c9d77
Guilherme Pedrosa Lima <guilherme.lima@engie.com>
2021-02-26 23:15:18 -0300

    deposit created

--- lib/rocketpay_web/router.ex
+++ lib/rocketpay_web/router.ex
@@ -13,6 +13,10 @@ defmodule RocketpayWeb.Router do
     get "/welcome", WelcomeController, :welcome_message
 
     post "/users", UsersController, :create
+
+    post "/accounts/:id/deposit", AccountsController, :deposit
+    post "/accounts/:id/withdraw", AccountsController, :withdraw
+
   end
 
   # Enables LiveDashboard only for development
